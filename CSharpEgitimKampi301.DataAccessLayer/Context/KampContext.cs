c57cd9f6e9ba69d896bec6e75a774b866f036c16
Ataullah BEGGİ <ataullahbeggi@gmail.com>
2024-12-17 11:18:08 +0300

    2024 Murat YÜCEDAĞ Youtube C# Eğitim Kampı 301 Modülü

---
+++ CSharpEgitimKampi301.DataAccessLayer/Context/KampContext.cs
@@ -0,0 +1,24 @@
+﻿using CSharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.Context
+{
+    public class KampContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        // Category bizim C# tarafında kullanacak olduğumuz sınıfın ismi
+        // Categories ise SQL'e yansıyacak olan tablo ismi
+        // sınıfla tablo birbirinin içine girmesin ayırt edilsin diye yalın hali C#'ta class,çoğul hali SQL'de tablo olarak kullanılır.
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+
+    }
+}
---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
+
+/*
+ Field-Variable-Property
+ */
+
+/*
+ int x; --> Field bir değişken direkt sınıfın içine tanımlanırsa bu field
+ 
+public int y {get; set;} eğer sonuna get ve set alırsa bu property
+
+bir metod içinde tanımlanırsa variable
+void test()
+{
+    int z;
+}
+ */
+
+//CodeFirst yaklaşımında değerler veritabanına gönderilirken bunların Property olarak gönderilmesi gerekir
